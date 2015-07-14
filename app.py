0617aeeac5fa98c7ec895ee143d291df6054eea9
Nate Luzod <nate.luzod@mac.com>
2015-07-14 01:38:01 -1000

    [#8] Adding password protection to admin section

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+from flask.ext.basicauth import BasicAuth
  
 app = Flask(__name__)      
+
+app.config['BASIC_AUTH_USERNAME'] = 'hawaii'
+app.config['BASIC_AUTH_PASSWORD'] = 'trails'
  
 @app.route('/')
 def index():
   return render_template('home.html')
 
+basic_auth = BasicAuth(app)
+
 @app.route('/admin')
+@basic_auth.required
 def admin():
   return render_template('admin.html')
   
